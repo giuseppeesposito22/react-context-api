b4707db12f6aec7f8a2af4da45623f740c5dea6d
giuseppeesposito22 <gius.espo22@gmail.com>
2025-06-04 16:15:46 +0200

    add post provider

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import DefaultLayout from "./layouts/DefaultLayout";
 
 import path from "./data/path";
 
+import { PostProvider } from "./contexts/PostContext";
+
 // import delle pagine sotto il path posts
 
 import PostsListPage from "./pages/posts/PostsListPage";
@@ -16,20 +18,22 @@ import PostDetailPage from "./pages/posts/PostDetailPage";
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route path={path.HOME()} element={<HomePage />} />
-            <Route path={path.ABOUT()} element={<AboutPage />} />
+      <PostProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route path={path.HOME()} element={<HomePage />} />
+              <Route path={path.ABOUT()} element={<AboutPage />} />
 
-            {/* rotte che hanno in comune il prefisso posts */}
-            <Route path="/posts">
-              <Route path="" element={<PostsListPage />} />
-              <Route path=":id" element={<PostDetailPage />} />
+              {/* rotte che hanno in comune il prefisso posts */}
+              <Route path="/posts">
+                <Route path="" element={<PostsListPage />} />
+                <Route path=":id" element={<PostDetailPage />} />
+              </Route>
             </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </PostProvider>
     </>
   );
 }
